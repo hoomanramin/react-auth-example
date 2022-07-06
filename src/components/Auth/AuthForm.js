a20664c53741278f97a58b2abfda23e20265b502
Hooman <hoomanramin@gmail.com>
2022-07-06 17:07:38 +0430

    add sign in and sign up functionality

---
+++ src/components/Auth/AuthForm.js
@@ -0,0 +1,87 @@
+import {useState, useRef} from "react";
+import classes from "./AuthForm.module.css";
+
+const AuthForm = () => {
+  const emailInput = useRef();
+  const passwordInput = useRef();
+  const [isLogin, setIsLogin] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const [resData, setResData] = useState("");
+  const [error, setError] = useState("");
+
+  const switchAuthModeHandler = () => {
+    setIsLogin(prevState => !prevState);
+  };
+
+  const submitHandler = async e => {
+    e.preventDefault();
+    const enteredEmail = emailInput.current.value;
+    const enteredPassword = passwordInput.current.value;
+
+    setIsLoading(true);
+    let url;
+    if (isLogin) {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCty3-Luc0nbwy7Jn8M3O_U_Swk541cFrE";
+    } else {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCty3-Luc0nbwy7Jn8M3O_U_Swk541cFrE";
+    }
+    try {
+      const res = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify({
+          email: enteredEmail,
+          password: enteredPassword,
+          returnSecureToken: true,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (res.ok) {
+        const data = await res.json();
+        setResData(data);
+        setError("");
+        console.log(resData);
+      } else {
+        throw new Error("Authentication Failed");
+      }
+      setIsLoading(false);
+    } catch (err) {
+      setError(err.message);
+      setIsLoading(false);
+    }
+  };
+  return (
+    <section className={classes.auth}>
+      <h1>{isLogin ? "Login" : "Sign Up"}</h1>
+      <form onSubmit={submitHandler}>
+        <div className={classes.control}>
+          <label htmlFor="email">Your Email</label>
+          <input type="email" id="email" required ref={emailInput} />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="password">Your Password</label>
+          <input type="password" id="password" required ref={passwordInput} />
+        </div>
+        <div style={{color: "white"}}>{error}</div>
+        <div className={classes.actions}>
+          {!isLoading && (
+            <button>{isLogin ? "Login" : "Create Account"}</button>
+          )}
+          {isLoading && <p style={{color: "white"}}>Sending Request</p>}
+          <button
+            type="button"
+            className={classes.toggle}
+            onClick={switchAuthModeHandler}
+          >
+            {isLogin ? "Create new account" : "Login with existing account"}
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default AuthForm;
